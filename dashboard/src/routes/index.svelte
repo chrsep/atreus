d01b7ba3759fb145c4ce4b28c65c117232f7226a
Chrisando Pramudhita <chrsep@protonmail.com>
2021-02-17 18:31:14 +0700

    Revert "update deps."
    This reverts commit 2079a737

--- dashboard/src/routes/index.svelte
+++ dashboard/src/routes/index.svelte
@@ -47,4 +47,4 @@
 	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
-<p><strong >Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p><strong class="bg-black">Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
