260d3fadeba698a2260ade5e2c38a57ef66a33b1
Chrisando Pramudhita <chrsep@protonmail.com>
2021-02-21 14:23:05 +0700

    add more tables.

--- server/migrations/2021-02-18-084221_targets/up.sql
+++ server/migrations/2021-02-18-084221_targets/up.sql
@@ -3,4 +3,14 @@ create table targets (
     id uuid primary key,
     name varchar not null,
     created_at timestamp not null
+);
+
+create table scopes (
+    domain varchar not null primary key,
+    target uuid references targets on delete cascade
+);
+
+create table domains (
+    domain varchar primary key,
+    scope varchar references scopes on delete cascade
 )
--- server/src/schema.rs
+++ server/src/schema.rs
@@ -1,7 +1,30 @@
 table! {
+    domains (domain) {
+        domain -> Varchar,
+        scope -> Nullable<Varchar>,
+    }
+}
+
+table! {
+    scopes (domain) {
+        domain -> Varchar,
+        target -> Nullable<Uuid>,
+    }
+}
+
+table! {
     targets (id) {
         id -> Uuid,
         name -> Varchar,
         created_at -> Timestamp,
     }
 }
+
+joinable!(domains -> scopes (scope));
+joinable!(scopes -> targets (target));
+
+allow_tables_to_appear_in_same_query!(
+    domains,
+    scopes,
+    targets,
+);
