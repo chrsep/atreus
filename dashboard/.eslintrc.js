0e7d6d94820eff3be6c672eaf35c1cb641113b4e
Chrisando Pramudhita <chrsep@protonmail.com>
2021-06-29 14:46:10 +0700

    remove unused stuffs fix build.

--- dashboard/.eslintrc.js
+++ dashboard/.eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
   parserOptions: {
-    project: "./tsconfig.eslint.json"
+    project: "./tsconfig.eslint.json",
   },
   extends: [
+    "next",
     "airbnb-typescript",
     "airbnb/hooks",
     "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
   rules: {
     "react/react-in-jsx-scope": 0,
@@ -17,6 +18,6 @@ module.exports = {
     "jsx-a11y/anchor-is-valid": 0,
     "@typescript-eslint/no-use-before-define": ["error", { variables: false }],
     "import/prefer-default-export": 0,
-    "@typescript-eslint/no-redeclare": 0
-  }
+    "@typescript-eslint/no-redeclare": 0,
+  },
 }
