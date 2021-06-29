1cd52f934014d26437ed27af52097a76b627a106
Chrisando Pramudhita <chrsep@protonmail.com>
2021-06-29 14:51:25 +0700

    use jit.

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,11 +1,18 @@
+/* eslint-disable global-require,import/no-extraneous-dependencies */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  purge: ["src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+  ],
 }
