98465eff93209ea205695ec9e5fecd05683375e6
Chrisando Pramudhita <chrsep@protonmail.com>
2021-06-30 15:15:40 +0700

    add auth and ts paths.

--- dashboard/src/pages/_app.tsx
+++ dashboard/src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import "tailwindcss/tailwind.css"
 import { FC } from "react"
 import { AppProps } from "next/app"
+import { UserProvider } from "@auth0/nextjs-auth0"
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+const Atreus: FC<AppProps> = ({ Component, pageProps }) => (
+  <UserProvider>
+    <Component {...pageProps} />
+  </UserProvider>
 )
 
-export default MyApp
+export default Atreus
--- dashboard/src/pages/index.tsx
+++ dashboard/src/pages/index.tsx
@@ -1,3 +1,7 @@
+import { withPageAuthRequired } from "@auth0/nextjs-auth0"
+
 const Home = () => <div>Nothing to see here yet</div>
 
 export default Home
+
+export const getServerSideProps = withPageAuthRequired()
