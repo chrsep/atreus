e70e54ea3840092a0a5fd3fc444a36d3057aa0a2
Chrisando Pramudhita <chrsep@protonmail.com>
2021-07-17 18:19:26 +0700

    query alternative target domains.

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -16,13 +16,23 @@ async fn main() -> Result<(), ()> {
         .await
         .expect("failed to connect to database");
 
-    let root_domains = sqlx::query!(r#"SELECT * from "RootDomain""#)
+    let root_domains = sqlx::query!(r#"SELECT * from "RootDomain" where confirmed"#)
         .fetch_all(&pool)
         .await
         .expect("failed to query root domains");
 
     for domain in root_domains {
         let result = amass::intel(&domain.domain);
+        let company_id = vec![domain.companyId; result.len()];
+
+        let result = sqlx::query!(
+            r#"insert into "RootDomain" (domain, "companyId") SELECT * FROM UNNEST($1::text[], $2::int[]) ON CONFLICT DO NOTHING"#,
+            &result,
+            &company_id
+        )
+            .execute(&pool)
+            .await;
+
         print!("{} {:?}", domain.domain, result);
     }
 
