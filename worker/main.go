6958e1707abed76c8f369389c92ef6e3ca94f0bc
Chrisando Pramudhita <chrsep@protonmail.com>
2021-11-23 15:00:52 +0700

    make port scan faster.

--- worker/main.go
+++ worker/main.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"github.com/joho/godotenv"
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/prisma/prisma-client-go/runtime/transaction"
 	"golang.org/x/sync/semaphore"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 	"worker/db"
@@ -32,7 +34,7 @@ func run() error {
 	exit := make(chan string)
 
 	go runSubdomainEnumeration()
-	//go runPortScan()
+	go runPortScan()
 
 	for {
 		time.Sleep(time.Second * 10)
@@ -48,7 +50,7 @@ func run() error {
 
 func runSubdomainEnumeration() {
 	ctx := context.Background()
-	sem := semaphore.NewWeighted(10)
+	sem := semaphore.NewWeighted(5)
 	wg := &sync.WaitGroup{}
 
 	for {
@@ -72,61 +74,57 @@ func runSubdomainEnumeration() {
 	}
 }
 
-//func runPortScan() {
-//	for {
-//		log.Info("scanning services")
-//		domains, err := db.FindAllDomains()
-//		if err != nil {
-//			panic(err)
-//		}
-//
-//		domainNames := make([]string, 0)
-//		for _, domain := range domains {
-//			domainNames = append(domainNames, domain.Name)
-//		}
-//
-//		results, _ := scanServices(domainNames)
-//		for _, result := range results {
-//			service := db.ServiceModel{}
-//			service.Port, _ = strconv.Atoi(result.Port)
-//			service.Name = result.Webserver
-//			service.DomainName = result.Input
-//			service.ARecords = result.A
-//			service.CnameRecords = result.Cnames
-//
-//			response := db.ProbeResponseModel{}
-//			response.BodySHA = result.BodySha256
-//			response.URL = result.Url
-//			response.Host = result.Host
-//			response.Method = result.Method
-//			response.Scheme = result.Scheme
-//			response.Webserver = result.Webserver
-//			response.Timestamp = result.Timestamp
-//			response.Title = result.Title
-//			response.Header = result.ResponseHeader
-//			response.Body = result.ResponseBody
-//			response.StatusCode = result.StatusCode
-//			response.ContentType = result.ContentType
-//			response.Path = result.Path
-//			response.ResponseTime = result.ResponseTime
-//
-//			if _, err = db.UpsertService(service); err != nil {
-//				log.Error("failed to upsert service", err)
-//			}
-//
-//			if _, err = db.UpsertProbeResponse(response, service.Port, service.DomainName); err != nil {
-//				log.Error("failed to upsert probe response", err)
-//			}
-//
-//			if err := db.UpsertTech(result.Technologies, response.BodySHA); err != nil {
-//				log.Error("failed to upsert tech", err)
-//			}
-//		}
-//
-//		log.Info("port-scan: 5s timeout for port scan")
-//		time.Sleep(time.Second * 5)
-//	}
-//}
+func runPortScan() {
+	for {
+		log.Info("scanning services")
+		domains, err := db.FindAllDomains()
+		if err != nil {
+			panic(err)
+		}
+
+		domainNames := make([]string, 0)
+		for _, domain := range domains {
+			domainNames = append(domainNames, domain.Name)
+		}
+
+		var ops []transaction.Param
+		results, _ := scanServices(domainNames)
+		for _, result := range results {
+			service := db.ServiceModel{}
+			service.Port, _ = strconv.Atoi(result.Port)
+			service.Name = result.Webserver
+			service.DomainName = result.Input
+			service.ARecords = result.A
+			service.CnameRecords = result.Cnames
+
+			response := db.ProbeResponseModel{}
+			response.BodySHA = result.BodySha256
+			response.URL = result.Url
+			response.Host = result.Host
+			response.Method = result.Method
+			response.Scheme = result.Scheme
+			response.Webserver = result.Webserver
+			response.Timestamp = result.Timestamp
+			response.Title = result.Title
+			response.Header = result.ResponseHeader
+			response.Body = result.ResponseBody
+			response.StatusCode = result.StatusCode
+			response.ContentType = result.ContentType
+			response.Path = result.Path
+			response.ResponseTime = result.ResponseTime
+
+			ops = append(ops, db.UpsertService(service))
+			ops = append(ops, db.UpsertProbeResponse(response, service.Port, service.DomainName))
+			ops = append(ops, db.UpsertTech(result.Technologies, response.BodySHA)...)
+		}
+		if err := db.RunTransactions(ops); err != nil {
+			log.Error("failed to save port scan results", err)
+		}
+
+		log.Info("port-scan: 5s timeout for port scan")
+		time.Sleep(time.Second * 5)
+	}
+}
 
 func enumerateSubdomain(domain string, id int, sem *semaphore.Weighted, wg *sync.WaitGroup) {
 	defer func() {
