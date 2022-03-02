346ef3cd7de1776262e3351aad0641995c5c74e1
Chrisando E. Pramudhita <chrsep@protonmail.com>
2022-03-02 19:18:49 +0700

    update deps.

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,24 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // next.config.js
 const withPreact = require("next-plugin-preact")
-const withPWA = require("next-pwa")
 const withPlugins = require("next-compose-plugins")
-
-const nextPWAConfig = {
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    // disable: process.env.NODE_ENV === 'development',
-    // register: true,
-    // scope: '/app',
-    // sw: 'service-worker.js',
-    // ...
-  },
-}
 
 /** @type {import("next").NextConfig} */
 const config = {
-  swcMinify: true,
+  swcMinify: true
 }
 
-module.exports = withPlugins([[withPWA, nextPWAConfig], withPreact], config)
+module.exports = withPlugins([withPreact], config)
